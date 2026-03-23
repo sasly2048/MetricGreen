b30d1d22f1fc6eb1b6157df2d0f22a0e06a8b897
Raghavendra G <raghavendrasujith204800@gmail.com>
2026-03-23 20:38:21 +0530

    chore: ignore eslint errors during production builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
