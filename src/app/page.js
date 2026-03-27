aa24af9021bf3155fe401478243dc8ed4cff32ef
Raghavendra G <raghavendrasujith204800@gmail.com>
2026-03-27 17:21:50 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ const contractABI = [
 
 const contractAddress =
   process.env.NEXT_PUBLIC_METRIC_GREEN_CONTRACT_ADDRESS ||
-  "0x3A60C10BAcCF9b0DA9F14b26abC0Ce891722867E";
+  "0x336b85fBf799ca5fa67119aCd918B396E6EAbfcE";
 const requiredChainId = parseChainId(
   process.env.NEXT_PUBLIC_METRIC_GREEN_CHAIN_ID,
   11155111n,
